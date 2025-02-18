a3a5e6c47e99051ee872e736461bddad10b30004
Rutuja <patil.rut@northeastern.edu>
2025-02-18 18:57:50 -0500

    styling the navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Image from 'next/image'
 import logo from '@/assets/images/logo-white.png'
 import profile from '@/assets/images/profile.png'
+import Link from 'next/link'
+import {FaGoogle} from 'react-icons/fa'
 
 const Navbar = () => {
   return (
@@ -40,7 +42,7 @@ const Navbar = () => {
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
           >
             
-            <a className="flex flex-shrink-0 items-center" href="/index.html">
+            <Link className="flex flex-shrink-0 items-center" href="/">
               <Image
                 className="h-10 w-auto"
                 src={logo}
@@ -50,24 +52,22 @@ const Navbar = () => {
               <span className="hidden md:block text-white text-2xl font-bold ml-2"
                 >PropertyPulse</span
               >
-            </a>
+            </Link>
            
             <div className="hidden md:ml-6 md:block">
               <div className="flex space-x-2">
-                <a
-                  href="/index.html"
+                <Link
+                  href="/"
                   className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home</a
-                >
-                <a
-                  href="/properties.html"
+                  >Home</Link>
+                <Link
+                  href="/properties"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Properties</a
-                >
-                <a
-                  href="/add-property.html"
+                  >Properties</Link>
+                <Link
+                  href="/properties/add"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Property</a
+                  >Add Property</Link
                 >
               </div>
             </div>
@@ -79,7 +79,7 @@ const Navbar = () => {
               <button
                 className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
               >
-                <i className="fa-brands fa-google text-white mr-2"></i>
+                <FaGoogle className='text-white mr-2 '/>
                 <span>Login or Register</span>
               </button>
             </div>
@@ -89,7 +89,7 @@ const Navbar = () => {
           <div
             className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0"
           >
-            <a href="messages.html" className="relative group">
+            <Link href="/messages" className="relative group">
               <button
                 type="button"
                 className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -117,7 +117,7 @@ const Navbar = () => {
                 2
                 
               </span>
-            </a>
+            </Link>
 
             <div className="relative ml-3">
               <div>
@@ -130,9 +130,9 @@ const Navbar = () => {
                 >
                   <span className="absolute -inset-1.5"></span>
                   <span className="sr-only">Open user menu</span>
-                  <img
+                  <Image
                     className="h-8 w-8 rounded-full"
-                    src="/images/profile.png"
+                    src={profile}
                     alt=""
                   />
                 </button>
@@ -147,22 +147,20 @@ const Navbar = () => {
                 aria-labelledby="user-menu-button"
                 tabIndex="-1"
               >
-                <a
-                  href="/profile.html"
+                <Link
+                  href="/profile"
                   className="block px-4 py-2 text-sm text-gray-700"
                   role="menuitem"
                   tabIndex="-1"
                   id="user-menu-item-0"
-                  >Your Profile</a
-                >
-                <a
-                  href="/saved-properties.html"
+                  >Your Profile</Link>
+                <Link
+                  href="/properties/saved"
                   className="block px-4 py-2 text-sm text-gray-700"
                   role="menuitem"
                   tabIndex="-1"
                   id="user-menu-item-2"
-                  >Saved Properties</a
-                >
+                  >Saved Properties</Link>
                 <button
                   className="block px-4 py-2 text-sm text-gray-700"
                   role="menuitem"
@@ -180,21 +178,18 @@ const Navbar = () => {
      
       <div className="hidden" id="mobile-menu">
         <div className="space-y-1 px-2 pb-3 pt-2">
-          <a
-            href="/index.html"
+          <Link
+            href="/"
             className="bg-black text-white block rounded-md px-3 py-2 text-base font-medium"
-            >Home</a
-          >
-          <a
-            href="/properties.html"
+            >Home</Link>
+          <Link
+            href="/properties"
             className="text-white block rounded-md px-3 py-2 text-base font-medium"
-            >Properties</a
-          >
-          <a
-            href="/add-property.html"
+            >Properties</Link>
+          <Link
+            href="/properties/add"
             className="text-white block rounded-md px-3 py-2 text-base font-medium"
-            >Add Property</a
-          >
+            >Add Property</Link>
           <button
             className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
           >
